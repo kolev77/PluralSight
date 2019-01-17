6fbf6501d1a7720b51bfb1b4c0b1025467e7d42f
kolev77 <rostislav.kolev77@gmail.com>
2019-01-17 11:43:58 +0200

    Design Patterns & Angular

---
+++ JavaSE/DesignPatterns/ConcurrencyAndMultiThreading/src/_01_Introduction/_1_raceCondition_/LongWrapper.java
@@ -0,0 +1,23 @@
+package _01_Introduction._1_raceCondition_;
+
+public class LongWrapper {
+
+	private Object key = new Object();
+	private long l;
+
+	public LongWrapper(long l) {
+		this.l = l;
+	}
+
+	public long getValue() {
+		synchronized (key) {
+			return l;
+		}
+	}
+
+	public void incrementValue() {
+		synchronized (key) {
+			l = l + 1;
+		}
+	}
+}
---
+++ JavaSE/DesignPatterns/ConcurrencyAndMultiThreading/src/_04_/runnable/RaceCondition.java
@@ -0,0 +1,31 @@
+package _04_.runnable;
+
+
+import _04_.runnable.model.LongWrapper;
+
+class RaceCondition {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		LongWrapper longWrapper = new LongWrapper(0L);
+		
+		Runnable r = () -> {
+			
+			for (int i = 0 ; i < 1_000 ; i++) {
+				longWrapper.incrementValue();
+			}
+		};
+		
+		Thread[] threads = new Thread[1_000];
+		for (int i = 0 ; i < threads.length ; i++) {
+			threads[i] = new Thread(r);
+			threads[i].start();
+		}
+		
+		for (int i = 0 ; i < threads.length ; i++) {
+			threads[i].join();
+		}
+		
+		System.out.println("Value = " + longWrapper.getValue());
+	}
+}
